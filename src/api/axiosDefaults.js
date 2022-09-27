6455a98ddf6a536efcdb47ce3649ce758aa49615
Lee-moran <leemoran06@hotmail.com>
2022-09-27 15:13:00 +0000

    changes made still stuck

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://django-api-frame.herokuapp.com/";
+axios.defaults.baseURL = "https://8000-leemoran-drfapi-yyhhxw8xrnz.ws-eu67.gitpod.io/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
 export const axiosReq = axios.create();
 export const axiosRes = axios.create();
+
+// "https://django-api-frame.herokuapp.com/"; was OG baseURL 
