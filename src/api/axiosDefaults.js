a9a2d378c1442dc34a70300a182be9bd78897cc5
Lee-moran <leemoran06@hotmail.com>
2022-09-30 15:36:31 +0000

    updated with new heroku app

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://8000-leemoran-drfapi-yyhhxw8xrnz.ws-eu67.gitpod.io/";
+axios.defaults.baseURL = "https://api-moments-new.herokuapp.com";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+axios.defaults.headers.post['Access-Control-Allow-Origin'] = "*";
 axios.defaults.withCredentials = true;
 
 export const axiosReq = axios.create();
 export const axiosRes = axios.create();
-
-// "https://django-api-frame.herokuapp.com/"; was OG baseURL 
