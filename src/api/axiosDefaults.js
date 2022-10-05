34a31a401a4ed6d8cae6289d19bed9e0c82524f0
Lee-moran <leemoran06@hotmail.com>
2022-10-05 14:43:43 +0000

    update and save

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://api-moments-new.herokuapp.com";
+axios.defaults.baseURL = "https://api-moments-new.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
-axios.defaults.headers.post['Access-Control-Allow-Origin'] = "*";
+// axios.defaults.headers.post['Access-Control-Allow-Origin'] = "*";
 axios.defaults.withCredentials = true;
 
 export const axiosReq = axios.create();
